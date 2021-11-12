10323d7d2137aa035c237da2171dc8c5568b4323
Kong Yue Chen <kongyue.chen@gmail.com>
2021-11-12 14:37:45 -0500

    Merging

--- client/store/drinks.js
+++ client/store/drinks.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-
-
-//Action Type
-const FETCH_DRINKS = 'FETCH_DRINKS'
-
-
-//Action Creator
-const fetchDrinks = (drinks) =>{
-  return {
-    type: fetchDrinks,
-    drinks
-  }
-}
-
-//Thunk Action Creator
-export const fetchingDrinks = () => {
-  return async (dispatch) => {
-    const {data: drinks} = await axios.get('/api/drinks')
-    dispatch(fetchDrinks(drinks))
-  }
-}
-
-
-//Drinks Sub-reducer
-export default function drinks(state = [], action) {
-  switch (action.type) {
-    case FETCH_DRINKS:
-      return action.drinks
-=======
 import axios from "axios";
 
 const GET_DRINKS = 'GET_DRINKS';
@@ -51,7 +20,6 @@ export default function drinksReducer(state=[], action){
   switch(action.type){
     case GET_DRINKS:
       return action.drinksFromServer;
->>>>>>> ada904d8a1be9a89614389040fdfd49e63de48ef
     default:
       return state;
   }
