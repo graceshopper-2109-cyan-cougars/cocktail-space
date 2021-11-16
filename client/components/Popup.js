e14ff23f5840fbd32282cc4ff5be5e80e5a23f6b
David Kremenitzer <dkrem21@gmail.com>
2021-11-16 16:25:13 -0500

    local storage set for popup/favicon update

--- client/components/Popup.js
+++ client/components/Popup.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Popup(props) {
+  useEffect(() => {
+    let pop_status = localStorage.getItem('pop_status');
+    if (!pop_status) {
+      props.setTrigger(true);
+      localStorage.setItem('pop_status', 1);
+    }
+  }, []);
+
   return props.trigger ? (
     <div className='popup'>
       <div className='popup-inner'>
