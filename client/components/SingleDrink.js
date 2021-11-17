24d1f50f8fc3a09b1c352a6ef1fcca17e22f3f92
Kong Yue Chen <kongyue.chen@gmail.com>
2021-11-17 18:34:33 -0500

    Final Commit

--- client/components/SingleDrink.js
+++ client/components/SingleDrink.js
@@ -5,12 +5,6 @@ import history from '../history';
 const SingleDrink = (props) => {
   const { name, price, image, baseLiquor, description } = props.drink;
   return (
-    // <div className='singleDrink'>
-    //   <img src={image}></img>
-    //   <p>{name}</p>
-    //   <p>{price}</p>
-    //   <p>{baseLiquor}</p>
-    // </div>
     <div className='card'>
       <div className='card-header'>
         <img src={image} alt={name} />
@@ -19,7 +13,6 @@ const SingleDrink = (props) => {
         <span className='tag tag-black'>{baseLiquor}</span>
         <h4>{name}</h4>
         <p>{description}</p>
-        {/* <Link to={`/drinks/${props.drink.id}`}> */}
         <button
           className='quizButton'
           onClick={() => {
